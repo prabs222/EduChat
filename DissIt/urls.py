c529c2e9e4a21bdf9f52f21969a73f4b79dcc85a
Prabhakar Mishra <prabhakarmishra222@gmail.com>
2023-11-28 02:39:39 +0530

    added static url configuration

--- DissIt/urls.py
+++ DissIt/urls.py
@@ -3,6 +3,7 @@ from django.urls import path, include
 from core import urls
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -10,3 +11,5 @@ urlpatterns = [
     path('api/',include('core.api.urls')),
     
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns += staticfiles_urlpatterns()
